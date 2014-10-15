d972dd0936b226150fe135c86d59b8a997531c77
Skander Jabouzi <jabouzi@gmail.com>
2014-10-15 01:08:56 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/paypal/trunk@46 ccb7fb94-c968-4e40-bbd2-4ad4172dcf45

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,7 @@ session_start();
 
 $root = $_SERVER['DOCUMENT_ROOT'].'/';
 $root = str_replace('//', '/', $root);
-ini_set('include_path',ini_get('include_path').':'.$root.'lib:');
-var_dump(ini_get('include_path'));
+ini_set('include_path',ini_get('include_path').':'.$root.'lib/:');
 
 include_once('db.php');
 include_once('functions.php');
