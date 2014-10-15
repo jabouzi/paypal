ff6feb2c77e006e952c0b7221077eb657cdfd289
Skander Jabouzi <jabouzi@gmail.com>
2014-10-15 02:19:45 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/paypal/trunk@54 ccb7fb94-c968-4e40-bbd2-4ad4172dcf45

---
+++ cancel.php
@@ -0,0 +1,41 @@
+<?php include('includes/config.php'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="UTF-8">
+		<meta http-equiv="cache-control" content="public">
+		<meta http-equiv="expires" content="Fri, 30 Dec 2013 12:00:00 GMT">
+
+		<title>Paypal Payments</title>
+		<meta name="description" content="Shopping cart">
+		<meta name="keywords" content="">
+		<meta name="robots" content="index,follow">
+		<meta name="author" content="Skander Software Solutions">
+		<link rel="stylesheet" type="text/css" media="screen, projection" href="assets/reset.css">
+		<link rel="stylesheet" type="text/css" media="screen, projection" href="assets/paypal.css">
+		<link rel="stylesheet" type="text/css" media="print" href="assets/print.css">d
+	</head>
+	<body>
+		<div id="wrap">
+			<div id="shop">
+				<div id="wrapper">
+					<section class="section">
+						<div class="section_inner fixed">
+							<h2 class="main">Paypal Payments</h2>
+							<section id="step3">
+								<p class="instructions">
+									please fill in the necessary information so we can complete your purchase.</p>s
+
+									<footer>
+										<p class="notice_click_once"></p>
+									</footer>
+								</form>
+							</section>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
