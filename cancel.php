ff6b011d2f889865a990e282ba6094989f91c0c6
Skander Jabouzi <jabouzi@gmail.com>
2014-10-19 11:43:11 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/paypal/trunk@72 ccb7fb94-c968-4e40-bbd2-4ad4172dcf45

--- cancel.php
+++ cancel.php
@@ -1,6 +1,7 @@
 <?php
 var_dump($_GET);
 include('includes/config.php'); 
+
 $_SESSION['paypal_message'] = 'You cancelled your paypal transaction, click <a href="indx.php">here</a>to try again.';
 actionPaypalCancel();
 header('location: complete.php');
