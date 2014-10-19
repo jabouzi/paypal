0fb4756e41028d74e23203773be5f40023d43cea
Skander Jabouzi <jabouzi@gmail.com>
2014-10-19 17:05:48 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/paypal/trunk@104 ccb7fb94-c968-4e40-bbd2-4ad4172dcf45

--- process.php
+++ process.php
@@ -1,4 +1,4 @@
-<pre><?php
+<?php
 
 include('includes/config.php');
 
@@ -8,6 +8,6 @@ if (isset($_SESSION['PayerAddress']['shipping'])) $shipping_id = $billing_id;
 else $shipping_id = save_shipping_address();
 save_order($billing_id, $shipping_id, $_SESSION['Creditcard']['CardType'], $_SESSION['Creditcard']['Price']);
 $_SESSION['result'] = actionConfirm();
-var_dump($_SESSION['result']);
-//header('Location: '.$configuration['siteurl'].'complete.php');
-//exit();
+$_SESSION['result'];
+header('Location: '.$configuration['siteurl'].'complete.php');
+exit();
