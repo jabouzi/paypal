2d3911a19d001d483a52b88d2a4793a3016f3087
Skander Jabouzi <jabouzi@gmail.com>
2014-12-22 03:58:58 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/paypal/trunk@142 ccb7fb94-c968-4e40-bbd2-4ad4172dcf45

--- process.php
+++ process.php
@@ -10,7 +10,7 @@ save_order($billing_id, $shipping_id, $_SESSION['Creditcard']['CardType'], $_SES
 $_SESSION['result'] = actionConfirm();
 $_SESSION['result'];
 $template = file_get_contents('templates/email.txt');
-$message = sprintf($template, $_SESSION['result'], ($_SESSION['Creditcard']['Price']+$_SESSION['Creditcard']['Shipping']));
+$message = sprintf($template, $_SESSION['result'], money_format('%n', ($_SESSION['Creditcard']['Price']+$_SESSION['Creditcard']['Shipping'])));
 var_dump(send_email($_SESSION['PayerAddress']['email'], $message));
 //header('Location: '.$configuration['siteurl'].'complete.php');
 //exit();
